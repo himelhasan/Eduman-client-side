ca857bc1c6bfb1069ed5ce4b9fef0bac6120ea08
Himel Hasan <himelhasan497@gmail.com>
2022-10-26 12:10:08 +0600

    server push

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA6Z_ssVGaX3IuWCrWTdjNQFM26_m7QFIE",
-  authDomain: "eduman-d61ba.firebaseapp.com",
-  projectId: "eduman-d61ba",
-  storageBucket: "eduman-d61ba.appspot.com",
-  messagingSenderId: "1482168409",
-  appId: "1:1482168409:web:08e6cfc7d9b40b8fcbf948",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
